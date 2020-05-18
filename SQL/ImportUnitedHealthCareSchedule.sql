eac8f75a1caba1422d5ab13e76a8d859d5e440ab
Cody Rissler <crissler@mdclarity.com>
2020-05-18 16:47:47 -0500

    more updates

--- SQL/ImportUnitedHealthCareSchedule.sql
+++ SQL/ImportUnitedHealthCareSchedule.sql
@@ -1,17 +1,11 @@
 USE [PPPSW_Contract]
 GO
 
-ALTER TABLE [dbo].[ImportUnitedHealthCareSchedule] DROP CONSTRAINT [DF__ImportUni__RowDa__2630A1B7]
-GO
-
-ALTER TABLE [dbo].[ImportUnitedHealthCareSchedule] DROP CONSTRAINT [DF__ImportUni__Recor__253C7D7E]
-GO
-
-/****** Object:  Table [dbo].[ImportUnitedHealthCareSchedule]    Script Date: 5/18/2020 2:50:55 PM ******/
+/****** Object:  Table [dbo].[ImportUnitedHealthCareSchedule]    Script Date: 5/18/2020 3:19:34 PM ******/
 DROP TABLE [dbo].[ImportUnitedHealthCareSchedule]
 GO
 
-/****** Object:  Table [dbo].[ImportUnitedHealthCareSchedule]    Script Date: 5/18/2020 2:50:55 PM ******/
+/****** Object:  Table [dbo].[ImportUnitedHealthCareSchedule]    Script Date: 5/18/2020 3:19:34 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -27,20 +21,8 @@ CREATE TABLE [dbo].[ImportUnitedHealthCareSchedule](
 	[PlaceOfService] [varchar](100) NULL,
 	[Amount] [decimal](18, 2) NULL,
 	[StartDate] [date] NULL,
-	[EndDate] [date] NULL,
-	[RecordID] [uniqueidentifier] NOT NULL,
-	[RowDate] [datetime] NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[RecordID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	[EndDate] [date] NULL
 ) ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[ImportUnitedHealthCareSchedule] ADD  DEFAULT (newid()) FOR [RecordID]
-GO
-
-ALTER TABLE [dbo].[ImportUnitedHealthCareSchedule] ADD  DEFAULT (getdate()) FOR [RowDate]
 GO
 
 
---
+++ SQL/TransfromUnitedHealthCareSchedule.sql
@@ -0,0 +1,36 @@
+USE [PPPSW_Contract]
+GO
+
+/****** Object:  Table [dbo].[ImportUnitedHealthCareSchedule]    Script Date: 5/18/2020 3:17:34 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[TransfromUnitedHealthCareSchedule](
+	[Fname] [varchar](100) NULL,
+	[Code] [varchar](50) NULL,
+	[Modifier] [varchar](50) NULL,
+	[Remark] [varchar](100) NULL,
+	[TypeOfService] [varchar](100) NULL,
+	[PlaceOfService] [varchar](100) NULL,
+	[Amount] [decimal](18, 2) NULL,
+	[StartDate] [date] NULL,
+	[EndDate] [date] NULL,
+	[RecordID] [uniqueidentifier] NOT NULL,
+	[RowDate] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[RecordID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[TransfromUnitedHealthCareSchedule] ADD  DEFAULT (newid()) FOR [RecordID]
+GO
+
+ALTER TABLE [dbo].[TransfromUnitedHealthCareSchedule] ADD  DEFAULT (getdate()) FOR [RowDate]
+GO
+
+
